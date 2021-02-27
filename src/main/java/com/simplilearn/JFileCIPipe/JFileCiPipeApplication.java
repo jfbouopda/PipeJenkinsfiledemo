c5a9abaa37dd24123b94804e4b6988a3b5121bff
student <jfbouopda@gmail.com>
2021-02-27 17:09:04 +0000

    first commit

---
+++ src/main/java/com/simplilearn/JFileCIPipe/JFileCiPipeApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JFileCIPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JFileCiPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JFileCiPipeApplication.class, args);
+	}
+
+}
